3477feba0efc0dfaa40e129d45c9575f255c5790
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-28 21:23:14 -0300

    feat: get and load user data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,14 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -8,12 +16,23 @@ type AuthProviderProps = {
 
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
   isLogging: boolean;
+  user: User | null;
 };
+
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+};
+
+const USER_COLLECTION = '@gopizza:users';
 
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
   const [isLogging, setIsLogging] = useState(false);
 
   async function signIn(email: string, password: string) {
@@ -21,13 +40,38 @@ function AuthProvider({ children }: AuthProviderProps) {
       return Alert.alert('Login', 'Informe o e-mail e a senha.');
     }
 
-
     setIsLogging(true);
 
     auth()
       .signInWithEmailAndPassword(email, password)
       .then((account) => {
-        console.log(account);
+        firestore()
+          .collection('users')
+          .doc(account.user.uid)
+          .get()
+          .then(async (profile) => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin,
+              };
+
+              await AsyncStorage.setItem(
+                USER_COLLECTION,
+                JSON.stringify(userData)
+              );
+              setUser(userData);
+            }
+          })
+          .catch(() =>
+            Alert.alert(
+              'Login',
+              'Não foi possível buscar os dados de perfil do usuário.'
+            )
+          );
       })
       .catch((error) => {
         const { code } = error;
@@ -41,8 +85,31 @@ function AuthProvider({ children }: AuthProviderProps) {
       .finally(() => setIsLogging(false));
   }
 
+  async function loadUserStorageData() {
+    setIsLogging(true);
+
+    const storedUser = await AsyncStorage.getItem(USER_COLLECTION);
+
+    if (storedUser) {
+      const userdata = JSON.parse(storedUser) as User;
+      setUser(userdata);
+    }
+
+    setIsLogging(false);
+  }
+
+  async function signOut() {
+    await auth().signOut();
+    await AsyncStorage.removeItem(USER_COLLECTION);
+    setUser(null);
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signIn, isLogging }}>
+    <AuthContext.Provider value={{ signIn, signOut, isLogging, user }}>
       {children}
     </AuthContext.Provider>
   );
